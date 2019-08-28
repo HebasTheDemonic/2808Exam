e7dd626d87b27c5a4b480106ed3a6de81dc48059
HebasTheDemonic <44752168+HebasTheDemonic@users.noreply.github.com>
2019-08-28 21:09:12 +0300

    Add files via upload

---
+++ 2808Exam_Q1/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace _2808Exam_Q1
+{
+    class MainWindowViewModel
+    {
+        public ICommand command { get; set; }
+        public Double BtnWidth { get; set; }
+        public Double BtnHeight { get; set; }
+        public string BtnText { get; set; }
+        public MainWindowViewModel()
+        {
+            command = new Command();
+        }
+    }
+
+
+}
