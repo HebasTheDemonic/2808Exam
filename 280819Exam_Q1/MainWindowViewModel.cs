6e004923540a5f2374379130ccd5fc1568eb647c
HebasTheDemonic <44752168+HebasTheDemonic@users.noreply.github.com>
2019-08-30 08:41:53 +0300

    Add files via upload

---
+++ 280819Exam_Q1/MainWindowViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace _2808Exam_Q1
+{
+    class MainWindowViewModel
+    {
+        public DelegateCommand delegateCommand { get; set; }
+
+        public Double BtnWidth { get; set; }
+        public Double BtnHeight { get; set; }
+        public string BtnText { get; set; }
+
+        public MainWindowViewModel()
+        {
+            delegateCommand = new DelegateCommand(ExecuteMethod, canExecuteMethod);
+        }
+
+        private void ExecuteMethod()
+        {
+            ButtonParameterWindow buttonParameterWindow = new ButtonParameterWindow(BtnHeight, BtnWidth, BtnText);
+            buttonParameterWindow.Show();
+        }
+
+        private bool canExecuteMethod()
+        {
+            return true;
+        }
+    }
+
+
+}
---
+++ 280819Exam_Q1/WidthConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace _2808Exam_Q1
+{
+    public class WidthConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (System.Convert.ToInt32(value) < 150)
+            {
+                return "SMALL";
+            }
+            if (System.Convert.ToInt32(value) < 200)
+            {
+                return "MEDIUM";
+            }
+            if (System.Convert.ToInt32(value) < 250)
+            {
+                return "LARGE";
+            }
+            else
+            {
+                return "EXTRA LARGE";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
