b8ee4f4aae2015b11c0301a21069e1757cb3ed4d
HebasTheDemonic <44752168+HebasTheDemonic@users.noreply.github.com>
2019-09-01 10:50:32 +0300

    Add files via upload

---
+++ 280819Exam_Q2/GameInterfaceUserControlViewModel.cs
@@ -0,0 +1,110 @@
+﻿using Prism.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace _280819Exam_Q2
+{
+    class GameInterfaceUserControlViewModel : INotifyPropertyChanged
+    {
+        public DelegateCommand CorrectButtonPressDelegate { get; set; }
+        public DelegateCommand IncorrectButtonPressDelegate { get; set; }
+        public TimerValues TimerValues { get; set; }
+        public Brush _timerTextColor;
+        public Brush TimerTextColor
+        {
+            get
+            {
+                return _timerTextColor;
+            }
+            set
+            {
+                this._timerTextColor = value;
+                OnPropertyChanged("TimerTextColor");
+            }
+        }
+        public bool buttonPressFlag;
+        private Brush _gridBackground;
+
+        public Brush GridBackground
+        {
+            get
+            {
+                return _gridBackground;
+            }
+            set
+            {
+                this._gridBackground = value;
+                OnPropertyChanged("GridBackground");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public GameInterfaceUserControlViewModel()
+        {
+            TimerTextColor = Brushes.Black;
+            GridBackground = Brushes.White;
+            TimerValues = new TimerValues();
+            TimerValues.TimerValue = 30;
+            CorrectButtonPressDelegate = new DelegateCommand(ExecuteCommandCBP, canExecuteCommand);
+            IncorrectButtonPressDelegate = new DelegateCommand(ExecuteCommandIBP, canExecuteCommand);
+            buttonPressFlag = false;
+            Task.Run(() =>
+            {
+                while (TimerValues.TimerValue != 15)
+                {
+                    canExecuteCommand();
+                    Thread.Sleep(500);
+                }
+                while (TimerValues.TimerValue != 0)
+                {
+                    canExecuteCommand();
+                    Thread.Sleep(500);
+                    TimerTextColor = Brushes.Red;
+                }
+                TimerTextColor = Brushes.Black;
+                GridBackground = Brushes.Red;
+            });
+        }
+
+        private void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+        private void ExecuteCommandIBP()
+        {
+            buttonPressFlag = true;
+            TimerTextColor = Brushes.Black;
+            GridBackground = Brushes.Red;
+        }
+
+        private void ExecuteCommandCBP()
+        {
+            buttonPressFlag = true;
+            GridBackground = Brushes.Green;
+        }
+
+        private bool canExecuteCommand()
+        {
+            if (TimerValues.TimerValue == 0 || buttonPressFlag)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+    }
+}
---
+++ 280819Exam_Q2/TimerValue.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _280819Exam_Q2
+{
+    class TimerValues : INotifyPropertyChanged
+    {
+        private int _timerValue;
+
+        public int TimerValue
+        {
+            get
+            {
+                return _timerValue;
+            }
+            set
+            {
+                this._timerValue = value;
+                OnPropertyChanged("TimerValue");
+            }
+        }
+
+        private void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
